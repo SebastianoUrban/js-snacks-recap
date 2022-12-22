346e98ae0bf1941721d358a8fb7a3d0c8445464d
SebastianoUrban <sebastiano.urban98@gmail.com>
2022-12-22 15:33:32 +0100

    snack A - now we can sent message

---
+++ snack A/js/script.js
@@ -0,0 +1,28 @@
+/** Creare un input che permetta all'utente di inserire un messaggio e aggiungerlo ad una conversazione (tipo whatsapp).
+Dopo averlo aggiunto chiedere all'API una frase random (attraverso random/sentence) aggiungendo anch'essa al thread,
+ differenziando i messaggi utente da quelli del computer. **/
+
+const { createApp } = Vue;
+
+
+createApp({
+    data() {
+        return {
+            textNewMessage : '',
+            messageList : []
+        }
+    },
+    methods: {
+        sentMessage (text) {
+            if (text != '') {
+                this.messageList.push(
+                    {
+                        messageText : text,
+                        messageStatus : 'sent'
+                    }
+                );
+            }
+        }
+    }
+}).mount('#app');
+
